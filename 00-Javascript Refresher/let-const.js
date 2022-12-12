24543992d071e11c9490fb5c22bd6e6194cad229
Taha AYKIROĞLU <taha.aykiroglu@Gulsah-Dincer-MacBook-Pro.local>
2022-12-12 23:07:07 +0300

    Let & Const Section Added

---
+++ 00-Javascript Refresher/let-const.js
@@ -0,0 +1,14 @@
+// LET
+let myName = "Taha";
+console.log(myName);
+
+myName = "Talha";
+console.log(myName);
+
+// CONST 
+const mySurname = "Aykiroglu";
+console.log(mySurname);
+
+
+// const mySurname = "Aykirioglu"; 
+// that is not possible with const 
